97d576762c6c0b586ab20c9a57e9dfd6edd951d6
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-11-28 16:56:49 -0700

    style button

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,20 +3,22 @@ import { Button, Checkbox, Form } from "semantic-ui-react";
 
 const Login = () => {
   return (
-    <Form className="login">
-      <Form.Field>
-        <label>Username</label>
-        <input placeholder="Username" />
-      </Form.Field>
-      <Form.Field>
-        <label>Password</label>
-        <input placeholder="Password" />
-      </Form.Field>
-      <Form.Field>
-        <Checkbox label="I agree to the Terms and Conditions" />
-      </Form.Field>
-      <Button type="submit">Submit</Button>
-    </Form>
+    <React.Fragment className="login-card">
+      <Form className="login">
+        <Form.Field>
+          <label>Username</label>
+          <input placeholder="Username" />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input placeholder="Password" />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label="I agree to the Terms and Conditions" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </React.Fragment>
   );
 };
 
